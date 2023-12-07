127cf37b86b52a2831f7dc1f3cfd9de4d6835a94
掌君大大 <36527893+githubAysta@users.noreply.github.com>
2023-12-07 11:37:34 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,12 +4,11 @@ import { SubmitKey } from "../store/config";
 const isApp = !!getClientConfig()?.isApp;
 
 const cn = {
-  WIP: "该功能仍在开发中……",
-  Error: {
-    Unauthorized: isApp
-      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
-  },
+    WIP: "该功能仍在开发中……",
+    Error: {
+      Unauthorized:
+        "欢迎使用智能聊天助手！看起来您还没有填写访问码，或者填写了过期的访问码，您可以关注公众号《掌君大大》回复关键词《访问码》获取最新的访问码，然后[点击这里](/#/auth)填写访问码，即可开始使用ChatGPT"
+    },
   Auth: {
     Title: "需要密码",
     Tips: "管理员开启了密码验证，请在下方填入访问码",
